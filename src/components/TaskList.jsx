98051d8db4c94d91007bd55991d12432a292ec70
Shreekant Shreeram Yadav <shreekantyadav007@gmail.com>
2024-11-26 17:29:09 +0530

    Update TaskList.jsx

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -31,7 +31,7 @@ function TaskList() {
 
   const handleTaskDeleted = async (taskId) => {
     try {
-      await axios.delete(`/tasks/${taskId}`);
+      await axios.delete(`/tasks/delete/${taskId}`);
       setTasks(tasks.filter((task) => task._id !== taskId));
     } catch (err) {
       alert("Failed to delete task");
