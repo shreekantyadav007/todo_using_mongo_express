0bffc752420822fc6f54adff4de541d46996e0d9
Shreekant Shreeram Yadav <shreekantyadav007@gmail.com>
2024-12-09 12:37:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,28 @@ function App() {
   const handleTaskAdded = (newTask) => {
     setTasks([...tasks, newTask]);
   };
+  const searchTasks = async (query) => {
+    try {
+        const response = await axios.post('/tasks/search', {
+            query,
+        });
+        console.log('Search results:', response.data);
+    } catch (error) {
+        console.error('Error searching tasks:', error.response ? error.response.data : error.message);
+    }
+};
 
   return (
     <div className="container">
-      <h1>To-Do List</h1>
+      <h1>Create To-Do </h1>
       <AddTask onTaskAdded={handleTaskAdded} />
+       <br />
+      <h1>Search Todo</h1>
+          <input
+          type="text"
+          placeholder="Search tasks..."
+          onChange={(e) => searchTasks(e.target.value)}
+          />
       <TaskList tasks={tasks} />
     </div>
   );
